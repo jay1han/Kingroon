dcfc7b3858de95408af785d5532b74c039e60b05
Jay Han <jayhan.name@gmail.com>
2023-12-15 00:32:40 +0100

    Crash

--- Server/install.sh
+++ Server/install.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/bash
 
-mkdir /usr/share/octobox
-chmod a+w /usr/share/octobox
-cp octo*.py index.html /usr/share/octobox/
+mkdir -v /usr/share/octobox
+chmod -v a+w /usr/share/octobox
+cp -v octo*.py index.html /usr/share/octobox/
 touch /usr/share/octobox/octobox.lock
-chown -R octoprint:octoprint /usr/share/octobox
-chmod -R g+w /usr/share/octobox
-chmod a+w /usr/share/octobox/octobox.lock
-rm /var/www/html/index.html
-cp octocgi.py /var/www/bin/
+chown -v -R octoprint:octoprint /usr/share/octobox
+chmod -v -R g+w /usr/share/octobox
+chmod -v a+w /usr/share/octobox/octobox.lock
+rm -v /var/www/html/index.html
+cp -v octocgi.py /var/www/bin/
 
-cp octobox.service /etc/systemd/system/
+cp -v octobox.service /etc/systemd/system/
 systemctl daemon-reload
 systemctl enable octobox
 systemctl restart octobox
